ec98b7f6452a6076f5658148628f9773a0c01c62
Bendy Latortue <bendy.latortue@esih.edu>
2018-12-17 19:17:59 +0700

    second commit

--- launcher.py
+++ launcher.py
@@ -2,13 +2,16 @@ from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route('/v1/user/<name>')
-def index():
-    return "hello %" % userid
+@app.route('/v1/user/<userid>')
+def index(userid):
 
-@app.route('/admin',methods=['POST','GET'])
-def checkDate():
-    return 'From Date is'+request.args.get('from_date')+ ' To Date is '+ request.args.get('to_date')
+    return ' "user_id ":' + userid + '"app": "Airbnb","timestamp": "2018-10-29T00:30:12.984Z" '
+
+@app.route('/v1/app',methods=['POST','GET'])
+def information():
+
+	return 'user_id:'+request.args.get('user_id')+ ' app :'+ request.args.get('app')+' timestamp :' + request.args.get('timestamp')
+
 
 
 if __name__=="__main__":
